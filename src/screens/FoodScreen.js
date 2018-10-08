f6b43c7c23880109b970674d616b5e5ef268b763
Danny <dungtr@metropolia.fi>
2018-10-08 19:00:53 +0300

    Foods screen fixed

--- src/screens/FoodScreen.js
+++ src/screens/FoodScreen.js
@@ -19,25 +19,36 @@ export default class FoodScreen extends Component {
             </View>
 
             <View style={styles.reminder}>
-                <Text style={styles.nocolorWord}>Next meal in <Text style={styles.coloredWord}> 20 min </Text> | Daily calories left <Text style={styles.coloredWord}> 1000 </Text> </Text>
+                <Text style={styles.nocolorWord}>    Next meal in <Text style={styles.coloredWord}> 20 min </Text> | Daily calories left <Text style={styles.coloredWord}> 1000 </Text> </Text>
             </View>
-            <View style={styles.meal}>
-                <Text style={styles.nocolorWordTitle}>Suggested foods</Text>
-                <Text style={styles.coloredWord}>Broccoli</Text>
-                <Text style={styles.coloredWord}>Nuts</Text>
-                <Text style={styles.coloredWord}>Greek yogurt</Text>
+            <View style={styles.suggested}>
+                <Text style={styles.nocolorWordTitle}> Suggested foods</Text>
             </View>
-
-
-            <View style={styles.meal}>
-                <Text style={styles.nocolorWordTitle}>Latest foods</Text>
-                <Text style={styles.coloredWord}>Eggs</Text>
-                <Text style={styles.coloredWord}>Oatmeal</Text>
+            <View style={styles.suggestedContent}> 
+                <Text></Text>
+                <Text style={styles.coloredWord}> + Broccoli </Text>
+                <Text style={styles.coloredWord}> + Nuts </Text>
+                <Text style={styles.coloredWord}> + Greek yogurt </Text>
+                <Text></Text>
             </View>
 
-            <View style={styles.meal}>
-                <Text style={styles.nocolorWordTitle}>Favorite foods</Text>
-                <Text style={styles.coloredWord}>Eggs</Text>
+
+            <View style={styles.latest}>
+                <Text style={styles.nocolorWordTitle}> Latest foods</Text>
+            </View>
+            <View style={styles.latestContent}> 
+                <Text></Text>   
+                <Text style={styles.coloredWord}> + Eggs</Text>
+                <Text style={styles.coloredWord}> + Oatmeal</Text>
+                <Text></Text>
+            </View>
+
+            <View style={styles.favorite}>
+                <Text style={styles.nocolorWordTitle}> Favorite foods</Text>
+            </View>
+            <View style={styles.favoriteContent}> 
+                <Text></Text>   
+                <Text style={styles.coloredWord}> + Eggs</Text>
             </View>
 
 
@@ -57,10 +68,13 @@ export default class FoodScreen extends Component {
 const styles = StyleSheet.create({
 backgroundContainer: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(32,32,32,1)',
+    backgroundColor: '#262626',
     borderWidth: 1
   },  
+logoContainer: {
+    backgroundColor: 'rgba(32,32,32,1)',
+    flex:0.1,
+},  
 coloredWord: {
     color: 'rgba(80,198,209,0.8)',
     fontSize: 15
@@ -80,6 +94,29 @@ logoText: {
     alignSelf: 'center'
 },
 reminder: {
-    justifyContent: 'center'
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(32,32,32,1)',
+
+},
+suggestedContent: {
+    flexDirection: 'column',
+    flex: 0.2,
+    justifyContent: 'space-between',  
+},
+latestContent: {
+    flexDirection: 'column',
+    flex: 0.2,
+    justifyContent: 'space-between'
+},
+suggested: {
+    backgroundColor: 'rgba(32,32,32,1)',
+},
+latest: {
+    backgroundColor: 'rgba(32,32,32,1)',
+},
+favorite: {
+    backgroundColor: 'rgba(32,32,32,1)',
 }
+   
+
 });
